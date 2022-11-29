7c51e0023af72ad4c2a986ae67ac746451d1cbd0
masatoyu <sorrawittop1@gmail.com>
2022-11-30 03:23:11 +0700

    google login-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
 import '../styles/globals.css'
 import Layout from './components/layout'
-
+import {SessionProvider} from 'next-auth/react'
 // pages/_app.js
 import { Inter } from '@next/font/google'
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['Noto Sans Thai'] })
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, session }) {
   return (
-    <main className={inter.className}>
+    <SessionProvider session={session}>
+      <main className={inter.className}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
     </main>
+    </SessionProvider>
   )
 }
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -6,25 +6,49 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useSession, signIn, signOut } from 'next-auth/react'
+
 
 export default function ButtonAppBar() {
-  return (
-    <Box md={{ flexGrow: 1 }}>
-      <AppBar position="static" color="transparent" elevation={0}>
-        <Toolbar>
-       
-          <Typography variant="h6" component="div" align='center' sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
-            Logo
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  const { data: session } = useSession() 
+  if (session) {
+    return (
+      <Box md={{ flexGrow: 1 }}>
+        <AppBar position="static" color="transparent" elevation={0}>
+          <Toolbar>
+         
+            <Typography variant="h6" component="div" align='center' sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+              Logo
+            </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+            Welcome, {session.user.name}
+            </Typography>
+            <Button color="error" onClick ={ () => signOut()}>LOGOUT</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+    }else{
+      return (
+        <Box md={{ flexGrow: 1 }}>
+          <AppBar position="static" color="transparent" elevation={0}>
+            <Toolbar>
+           
+              <Typography variant="h6" component="div" align='center' sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                Logo
+              </Typography>
+            
+              <Button color="success" onClick ={ () => signIn()}>Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
+    }
+  
 }
